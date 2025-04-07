8553ae0d75f43f643260ef0150c13067bdc46f2c
parksangryong <parksangryong@Mac-mini-2.local>
2025-04-07 14:43:57 +0900

    암호화 로컬스토리지 설치

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,7 @@
 export {validateLogin, validateSignup} from './validate';
 export {mergeRefs} from './common';
+export {
+  setEncryptStorage,
+  getEncryptStorage,
+  removeEncryptStorage,
+} from './encryptStorage';
